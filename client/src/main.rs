9f8b060e89b26a331e6454a1712347023d949308
rhydiaan <rhydian.davies01@gmail.com>
2023-07-27 22:58:47 +0100

    Practicing some basic rust concepts (assigning variables and borrowship/ownership)

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
+    
+    let mut a: String = String::from("a");
+
+    let mut b: String = String::from("b");
+
+    println!("a is {} and b is {}", a, b);
+
+    a = String::from("c");
+    b = String::from("d");
+
+    println!("a is {} and b is {}", a, b);
+
+    fn yoink(a: &String, b: &String) {
+        println!("a is {} and b is {}", a, b);
+    }
+
+    yoink(&a, &b);
+    println!("a is {} and b is {}", a, b);
+    
     println!("Hello, world!");
 }
