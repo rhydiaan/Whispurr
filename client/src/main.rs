4b79f6b2008ac1c9d7ca48b4f7940f4526abd911
rhydiaan <rhydian.davies01@gmail.com>
2023-08-10 16:02:50 +0100

    feat: implementation of loggin in and creating user

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
+use url::Url;
+use tungstenite::{connect, Message};
 
 #[derive(FromPrimitive)]
 enum Inputs{
@@ -8,6 +10,12 @@ enum Inputs{
     Login = 1,
     SignUp = 2,
 }
+
+struct User {
+    username: String,
+    password: String,
+}
+
 
 
 fn main() {
@@ -49,19 +57,116 @@ fn user_choice() {
 }
 
 fn login(choice: &str) {
+
+    println!("To login please type your username:");
+
+    let mut username = String::new();
+
+    io::stdin().read_line(&mut username).expect("failed to readline");
+
+    let trimmed_username = username.trim();
+
+    if trimmed_username.is_empty() {
+        println!("\nInvalid input: Empty username");
+        return;
+    }
+
+    println!("And now password:");
+
+    let mut password = String::new();
+
+    io::stdin().read_line(&mut password).expect("failed to readline");
+
+    let trimmed_password = password.trim();
+
+    if trimmed_password.is_empty() {
+        println!("\nInvalid input: Empty password");
+        return;
+    }
+
+    let user = User {
+        username: String::from(trimmed_username),
+        password: String::from(trimmed_password),
+    };
+
+    let (mut socket, _) = connect(
+        Url::parse("wss://localhost:11000").unwrap()
+    ).expect("Can't connect");
     println!("\nYou chose Login with the choice {}", choice);
+    
+    let message = format!(
+        r#"{{
+            "Login" : {{
+                "id" : "{}",
+                "password" : "{}"
+            }}
+        }}"#, user.username, user.password);
+    
+
+    let _ = socket.send(Message::Text(message));
+    loop {
+        let msg = socket.read().expect("Error reading message");
+        println!("Received: {}", msg);
+    }
+    
 }
 
 fn sign_up(choice: &str) {
+
+    println!("To create a user please type your username:");
+
+    let mut username = String::new();
+
+    io::stdin().read_line(&mut username).expect("failed to readline");
+
+    let trimmed_username = username.trim();
+
+    if trimmed_username.is_empty() {
+        println!("\nInvalid input: Empty username");
+        return;
+    }
+
+    println!("And now password:");
+
+    let mut password = String::new();
+
+    io::stdin().read_line(&mut password).expect("failed to readline");
+
+    let trimmed_password = password.trim();
+
+    if trimmed_password.is_empty() {
+        println!("\nInvalid input: Empty password");
+        return;
+    }
+
+    let user = User {
+        username: String::from(trimmed_username),
+        password: String::from(trimmed_password),
+    };
+
+
+    let (mut socket, _) = connect(
+        Url::parse("wss://localhost:11000").unwrap()
+    ).expect("Can't connect");
     println!("\nYou chose Sign Up with the choice {}", choice);
+    
+    let message = format!(
+        r#"{{
+            "CreateUser" : {{
+                "id" : "{}",
+                "password" : "{}"
+            }}
+        }}"#, user.username, user.password);
+    
+
+    let _ = socket.send(Message::Text(message));
+    loop {
+        let msg = socket.read().expect("Error reading message");
+        println!("Received: {}", msg);
+    }
+    
 }
 
-// {
-// 	"CreateUser" : {
-// 		"id" : "user_id",
-// 		"password" : "password"
-// 	}
-// }
 
 // {
 // 	"Login" : {
